281dbcf68caeffcdcb7cc9a8a8a311f5173631fe
Tiago Pereira <tpereira1911@gmail.com>
2024-01-14 21:03:49 +0000

    Final

--- blockchain/contracts/VotingSystem.sol
+++ blockchain/contracts/VotingSystem.sol
@@ -5,27 +5,27 @@ pragma solidity >=0.4.22 <0.9.0;
 contract VotingSystem {
     address public owner;
     
-    // Structure to represent a candidate
+    // Estrutura para representar um candidato (Bloco)
     struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
 
-    // Mapping to store the candidates
+    // Mapeamento para guardar os candidatos
     mapping(uint => Candidate) public candidates;
 
-    // Mapping to check if an address has already voted
+    // Mapeamento para verificar se o endereço da carteira já votou 
     mapping(address => bool) public voters;
 
-    // Number of candidates
+    // Numero de candidatos
     uint public candidatesCount;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can perform this operation");
         _;
-    }
-
+    }	
+	//Definir o criador do contrato como o proprietário (owner).
     constructor() public {
         owner = msg.sender;
         addCandidate("Candidate 1");
@@ -33,13 +33,13 @@ contract VotingSystem {
     }
 
 
-    // Function to add a new candidate
+    // Função para adicionarum candidato
     function addCandidate(string memory _name) public onlyOwner {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
-    // Function to cast a vote
+    // Função para votar verificando se esta ainda não votou
     function vote(uint _candidateId) public {
         require(!voters[msg.sender], "You have already voted.");
         require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate ID.");
